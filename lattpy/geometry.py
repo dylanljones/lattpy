d6991237804678e7362a11660baefade660167a4
dylanljones <dylanljones94@gmail.com>
2020-12-20 17:02:08 +0100

    changed wigner-seitz computation to scipy Voronoi-graph

--- lattpy/geometry.py
+++ lattpy/geometry.py
@@ -1,8 +1,12 @@
 # coding: utf-8
 #
-# This code is part of pylattice.
-# 
+# This code is part of lattpy.
+#
 # Copyright (c) 2020, Dylan Jones
+#
+# This code is licensed under the MIT License. The copyright notice in the
+# LICENSE file in the root directory and this permission notice shall
+# be included in all copies or substantial portions of the Software.
 
 import numpy as np
 import itertools

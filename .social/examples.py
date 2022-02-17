1ee04cc74b345450975f3f0735706cebab3bacd0
dylanljones <dylanljones94@gmail.com>
2022-02-17 13:36:00 +0100

    update: add new example

--- .social/examples.py
+++ .social/examples.py
@@ -18,9 +18,26 @@ def plot_simple_square_periodic():
     return fig, ax
 
 
+def plot_graphene_periodic():
+    latt = lp.graphene()
+    latt.build((20.7, 10.5))
+    ax = latt.plot(legend=False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    fig = ax.get_figure()
+    fig.tight_layout()
+    fig.set_size_inches(4.85, 2.5)
+
+    return fig, ax
+
+
 def main():
-    fig, ax = plot_simple_square_periodic()
-    fig.savefig("example_square_periodic.png")
+    # fig, ax = plot_simple_square_periodic()
+    # fig.savefig("example_square_periodic.png")
+
+    fig, ax = plot_graphene_periodic()
+    fig.tight_layout()
+    fig.savefig("example_graphene_periodic.png", transparent=True)
     plt.show()
 
 
